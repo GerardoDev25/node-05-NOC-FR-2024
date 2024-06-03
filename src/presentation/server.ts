9bc6994d5b443c8a12d85bc612397ac8b4207503
Gerardo Miranda <gerardodev25@gmail.com>
2024-06-03 18:43:36 -0400

    feat(dev): :sparkles: add cron in order to manage time

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,12 @@
+import { CronJob } from 'cron';
+
 export class Server {
   static start() {
     console.log('Server started...');
+    const job = new CronJob('*/2 * * * * *', () => {
+      const date = new Date();
+      console.log(date);
+    });
+    job.start()
   }
 }

b57833cf7ec51c16f1b1f261d529fa1a5827d9c2
Gerardo Miranda <gerardodev25@gmail.com>
2024-06-03 20:16:13 -0400

    feat(dev): :sparkles: creating the check server use case

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,11 @@
+import { CheckService } from '../domain/use-cases/checks/check-service';
 import { CronService } from './cron/cron-service';
 
 export class Server {
   static start() {
     console.log('Server started...');
-    CronService.createJob('*/5 * * * * *', () => {});
+    CronService.createJob('*/5 * * * * *', () => {
+      new CheckService().execute('https://google.com')
+    });
   }
 }

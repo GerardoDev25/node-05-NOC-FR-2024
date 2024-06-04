0b6ee243064b7a6323856bce30aa44ca1e2118ec
Gerardo Miranda <gerardodev25@gmail.com>
2024-06-04 16:52:45 -0400

    chore(dev): implementing save logs funtionality

--- src/infrastruture/datasources/file-system.datasource.ts
+++ src/infrastruture/datasources/file-system.datasource.ts
@@ -5,7 +5,7 @@ import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
 
 export class FileSystemDataSource implements LogDataSource {
   private readonly logPath: string = 'logs/';
-  private readonly allLogsPaths = `logs/logs-low.log`;
+  private readonly allLogsPaths = `logs/logs-all.log`;
   private readonly mediumLogsPaths = `logs/logs-medium.log`;
   private readonly highLogsPaths = `logs/logs-high.log`;
 
@@ -27,9 +27,20 @@ export class FileSystemDataSource implements LogDataSource {
     );
   }
 
-  saveLog(log: LogEntity): Promise<void> {
-    throw new Error('Method not implemented.');
+  async saveLog(newLog: LogEntity): Promise<void> {
+    const logAsJson = `${JSON.stringify(newLog)}\n`;
+
+    fs.appendFileSync(this.allLogsPaths, logAsJson);
+    if (newLog.level === LogSeverityLevel.low) return;
+
+    if (newLog.level === LogSeverityLevel.medium) {
+      fs.appendFileSync(this.mediumLogsPaths, logAsJson);
+    }
+    else {
+      fs.appendFileSync(this.highLogsPaths, logAsJson);
+    }
   }
+  
   getLog(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
     throw new Error('Method not implemented.');
   }

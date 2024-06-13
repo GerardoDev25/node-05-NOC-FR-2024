0df31c00c2de954fe26de3f2566264151c2d8f65
Gerardo Miranda <gerardodev25@gmail.com>
2024-06-13 18:18:02 -0400

    chore(dev): :sparkles: recording log in multiples data sources

---
+++ src/domain/use-cases/checks/check-service-multiple.ts
@@ -0,0 +1,48 @@
+import { LogEntity, LogSeverityLevel } from '../../entities/log.entity';
+import { LogRepository } from '../../repository/log.repository';
+
+interface CheckServiceUseCaseMultiple {
+  execute: (url: string) => Promise<boolean>;
+}
+
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
+
+export class CheckServiceMultiple implements CheckServiceUseCaseMultiple {
+  constructor(
+    private readonly logRepository: LogRepository[],
+    private readonly successCallback: SuccessCallback,
+    private readonly errorCallback: ErrorCallback
+  ) {}
+
+  private callLogs(log: LogEntity) {
+    this.logRepository.forEach((logRep) => logRep.saveLog(log));
+  }
+  async execute(url: string): Promise<boolean> {
+    try {
+      const request = await fetch(url);
+      if (!request.ok) {
+        throw new Error(`Error on check service ${url}`);
+      }
+
+      const log = new LogEntity({
+        message: `service ${url} working`,
+        level: LogSeverityLevel.low,
+        origin: 'check-service.ts',
+      });
+      this.callLogs(log);
+      this.successCallback && this.successCallback();
+      return true;
+    } catch (error) {
+      const errorMessage = `${url} is not ok. ${error}`;
+      const log = new LogEntity({
+        message: errorMessage,
+        level: LogSeverityLevel.high,
+        origin: 'check-service.ts',
+      });
+      this.callLogs(log);
+      this.errorCallback && this.errorCallback(errorMessage);
+      return false;
+    }
+  }
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,7 @@
 import { envs } from '../config/plugin/envs.plugin';
 import { LogSeverityLevel } from '../domain/entities/log.entity';
 import { CheckService } from '../domain/use-cases/checks/check-service';
+import { CheckServiceMultiple } from '../domain/use-cases/checks/check-service-multiple';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource';
 import { MongoLogDataSource } from '../infrastructure/datasources/mongo-log.datasource';
@@ -14,7 +15,12 @@ const LogRepository = new LogRepositoryImpl(
   // new MongoLogDataSource()
   new PostgresLogDataSource()
 );
-const emailService = new EmailService();
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDataSource());
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDataSource());
+const postgresLogRepository = new LogRepositoryImpl(
+  new PostgresLogDataSource()
+);
+// const emailService = new EmailService();
 
 export class Server {
   static async start() {
@@ -37,7 +43,19 @@ export class Server {
     //   ).execute(url);
     // });
 
-    const logs = await LogRepository.getLogs(LogSeverityLevel.high)
-    console.log(logs);
+    // * multiple checkLos
+    
+    // CronService.createJob('*/5 * * * * *', () => {
+    //   // const url = 'http://localhost:3000/posts';
+    //   const url = 'https://google.com';
+    //   new CheckServiceMultiple(
+    //     [fsLogRepository, mongoLogRepository, postgresLogRepository],
+    //     () => console.log(`${url} is ok`),
+    //     (error) => console.error(error)
+    //   ).execute(url);
+    // });
+
+    // const logs = await LogRepository.getLogs(LogSeverityLevel.high);
+    // console.log(logs);
   }
 }
